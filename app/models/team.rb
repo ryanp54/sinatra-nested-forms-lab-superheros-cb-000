476b54f995ec12e0c767328310dcd2f7462aaeb0
5-4 <github email address>
2017-12-17 14:04:19 -0600

    Done.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,22 @@
 require_relative 'super_hero'
 
 class Team
-	attr_accessor :name
+	attr_accessor :name, :motto
+
+	@@members = []
 
 	def initialize(params)
 		@name = params[:name]
+		@motto = params[:motto]
+
+		params[:members].each do |member|
+			hero = SuperHero.new(member)
+			@@members << hero
+		end
+	end
+
+	def members
+		@@members		
 	end
 
 end
